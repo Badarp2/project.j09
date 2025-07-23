3ecbfc726ef9be70f1b0210be60fda6891604297
Badarp2 <waqas.badar9tv@gmail.com>
2025-07-23 04:51:24 -0700

    initial

---
+++ if-statement.js
@@ -0,0 +1,30 @@
+let baddar =50;
+
+if(baddar==90){
+document.write("you get A+ marks")
+}
+
+else if(baddar==80){
+
+document.write("you get A")
+}
+
+else if(baddar==70){
+
+document.write("you get B+" )
+}
+
+else if(baddar==60){
+
+document.write("you get B" )
+}
+
+else if(baddar==50){
+
+document.write("you get C" )
+}
+
+else{
+
+document.write("bc You are failed" )
+}
---
+++ object.js
@@ -0,0 +1,61 @@
+id[
+
+: 1,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+]
+    
+    id [: 2,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+]
+    
+    id : 3,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+    
+    id : 4,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+
+    {
+    id : 5,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+},
+    {
+    id : 6,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+},
+    {
+    id : 7,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+},
+    {
+    id : 8,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+},
+    {
+    id : 9,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+},
+    {
+    id : 10,
+    name : "TShirt",
+    price : 500,
+    desc : "this is product description"
+},
+console.log(Product[0].name)
